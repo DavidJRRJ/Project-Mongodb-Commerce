0eecd71b32d2641c43e196c6e2c076e4b021cdc7
David <david.lourenco94@gmail.com>
2022-12-15 17:29:31 -0300

    desafio 17

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -2,7 +2,7 @@ db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany(
   { tags: { $all: ["bovino"] } },
-  { $inc: { avaliacao: 5 } }
+  { $inc: { avaliacao: 5 } },
 );
 
 db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $inc: { avaliacao: 3 } });
