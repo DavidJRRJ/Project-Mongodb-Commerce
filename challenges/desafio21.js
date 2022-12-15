f80da6a97b257d9fe544ca14f115a2425306f9af
David <david.lourenco94@gmail.com>
2022-12-15 18:04:49 -0300

    desafio 23

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,6 +1,6 @@
 db.produtos.updateOne(
   { nome: "Cheddar McMelt" },
-  { $pop: { ingredientes: 1 } },
+  { $pop: { ingredientes: 1 } }
 );
 
-db.produtos.find({}, { nome: 1, ingredientes: 1, _id:0 });
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
